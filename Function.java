d0dbacea1a1d27832f146bee30523f02bd960e40
Nick Papadakis <nicholas.t.papadakis@gmail.com>
2016-03-09 18:58:45 -0800

    Minimally implemented Function

---
+++ Function.java
@@ -0,0 +1,27 @@
+import java.util.Point;
+
+public class Function
+{
+   private String function;
+
+   public Function(String function)
+   {
+      this.function = function;
+   }
+
+   public double getValue(double x)
+   {
+      return 0;
+   }
+
+   public Point calculateIntersection(Function other, double leftXBound, 
+      double rightXBound)
+   {
+      return null;
+   }
+
+   private double parse(String expression)
+   {
+      return 0;
+   }
+}
