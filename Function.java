c8df1b637f18c833be99f12d02882171b5ea8502
david <dpjames@calpoly.edu>
2016-03-10 15:11:00 -0800

    added variable replacement and test cases accordingly

--- Function.java
+++ Function.java
@@ -11,17 +11,24 @@ public class Function
 
    public double getValue(double x)
    {
-      return 0;
+      return Parser.parse(replaceVariables(x));
+   }
+   private String replaceVariables(double x)
+   {
+      String s = function;
+      for(int i = 0; i < s.length(); i++)
+      {
+         if(s.charAt(i) == 'x')
+         {
+            s = s.substring(0,i) + x + s.substring(i+1);
+         }
+      }
+      return s;
    }
 
    public Point calculateIntersection(Function other, double leftXBound, 
       double rightXBound)
    {
       return null;
-   }
-
-   private double parse(String expression)
-   {
-      return Parser.parse(expression);
    }
 }
